07d3ca601860edd3745a31b72a94a8250ead0172
NikolaGolubovic <nikolagolubovic985@gmail.com>
2021-12-27 23:54:28 +0100

    add env.example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import { database } from "./utils/database";
 
+const PORT = process.env.PORT || 5000;
+
 (async () => {
   await database.sequelize.sync({ alter: true }).then(() => {
-    app.listen(5000, () => {
-      console.log(`Server is running on port:5000`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port:${PORT}`);
     });
   });
 })();
